bf4b1c8e3577d18f1e1c17d334d72485071c340e
Nauman <naumanumer10@outlook.com>
2024-09-14 18:46:31 +0100

    Allowing The User To Roll The Dice

---
+++ src/Components/RollDice.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+function RollDice() {
+  const [currentDice,setCurrentDice]=useState(1);
+
+  const generateRandomNumber=(min,max)=>{
+  console.log (Math.floor(Math.random()*(max-min)+min))
+  setCurrentDice( Math.floor(Math.random()*(max-min)+min));
+  }
+  return (
+
+<DiceContainer>
+
+  <div className="dice">
+    <img onClick={()=>generateRandomNumber(1,7)}  
+     src={`/Images/dice_${currentDice}.png`} alt="dice1" />
+
+  </div>
+  <p>Click To Roll</p>
+</DiceContainer>
+    
+  )
+}
+
+export default RollDice
+
+const DiceContainer=styled.div`
+margin-top: 40px;
+display: flex;
+align-items: center;
+flex-direction: column;
+.dice{
+  cursor: pointer;
+}
+p{
+  font-weight: 500;
+  font-size: 30px;
+}
+`;
