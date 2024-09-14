d5a91020b008f82a59272f7419bfacb6979e43d0
Nauman <naumanumer10@outlook.com>
2024-09-14 18:52:57 +0100

    Updating  UI

--- src/Components/GamePlay.jsx
+++ src/Components/GamePlay.jsx
@@ -2,17 +2,36 @@ import React from 'react'
 
 import TotalScore from './TotalScore';
 import NumberSelector from './NumberSelector';
+import RollDice from './RollDice';
 
-
+import styled from 'styled-components';
 function GamePlay({toggle}) {
   return (
     <>
     <div>GamePlay</div>
     <button onClick={toggle}>click</button>
-    <TotalScore />
-    <NumberSelector/>
+    <MainContainer>
+      <div className='Top-section'>
+      <TotalScore />
+      <NumberSelector/>
+      </div>
+      <RollDice />
+    
+    </MainContainer>
+    
+   
     </>
   )
 }
 
 export default GamePlay
+
+const MainContainer=styled.div`
+padding: 70px;
+.Top-section{
+  display: flex;
+ justify-content: space-between;
+ align-items: end;
+}
+  
+`
