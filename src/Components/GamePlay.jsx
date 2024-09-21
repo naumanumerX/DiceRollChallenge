822c9a82f0a45b3542857c1ac9a111021695fdb4
Nauman <naumanumer10@outlook.com>
2024-09-21 18:21:59 +0100

    Updaing Front Page

--- src/Components/GamePlay.jsx
+++ src/Components/GamePlay.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import TotalScore from './TotalScore';
 import NumberSelector from './NumberSelector';
@@ -6,16 +6,31 @@ import RollDice from './RollDice';
 
 import styled from 'styled-components';
 function GamePlay({toggle}) {
+  const [currentDice,setCurrentDice]=useState(1);
+  const[selectedNumber,setSelectedNumber]=useState();
+  const[score,setScore]=useState(0);
+  const checkMate=()=>{
+
+    if(selectedNumber===currentDice){
+      setScore(prev=>prev+currentDice)
+
+    }
+    else{
+      setScore(prev=>prev-currentDice)
+
+    }
+  }
   return (
     <>
     <div>GamePlay</div>
     <button onClick={toggle}>click</button>
     <MainContainer>
       <div className='Top-section'>
-      <TotalScore />
-      <NumberSelector/>
+      <TotalScore score={score}/>
+      <NumberSelector selectedNumber={selectedNumber} setSelectedNumber={setSelectedNumber}/>
       </div>
-      <RollDice />
+      <RollDice  currentDice={currentDice} setCurrentDice={setCurrentDice} checkMate={checkMate}/>
+      
     
     </MainContainer>
     
